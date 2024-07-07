7ddf34ff029c6deb2aa3b3751c41650bff653c3b
dan <d@zurly.me>
2024-07-07 23:47:56 +0100

    rm api

--- app/api/ip/route.ts
+++
@@ -1,6 +0,0 @@
-import type { NextApiRequest } from "next";
-
-export async function GET(request: NextApiRequest) {
-  const ip = request.headers["x-forwarded-for"];
-  return new Response(JSON.stringify({ ip }));
-}
