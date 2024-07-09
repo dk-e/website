2061e5658629067f90ec29ee574fcc55dd66a362
dan <d@zurly.me>
2024-07-09 20:02:37 +0100

    blog page added

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,111 @@
+import type { Metadata } from "next";
+import { notFound } from "next/navigation";
+import { MDX } from "../[slug]/mdx";
+import { getBlogPostBySlug } from "../../../utils/blog";
+import { Undo2 } from "lucide-react";
+import { Link } from "next-view-transitions";
+
+const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata | undefined> {
+  const post = getBlogPostBySlug(params.slug);
+  if (!post) {
+    return;
+  }
+
+  const publishedTime = formatDate(post.metadata.date);
+
+  return {
+    title: post.metadata.title,
+    description: post.metadata.description,
+    openGraph: {
+      title: post.metadata.title,
+      description: post.metadata.description,
+      publishedTime,
+      type: "article",
+      url: `https://da.niel.lol/blog/${post.slug}`,
+      images: [
+        {
+          url: `https://da.niel.lol/og/blog?title=${post.metadata.title}&top=${publishedTime}`,
+        },
+      ],
+    },
+    twitter: {
+      title: post.metadata.title,
+      description: post.metadata.description,
+      card: "summary_large_image",
+      creator: "@lootings",
+      images: [
+        `https://da.niel.lol/og/blog?title=${post.metadata.title}&top=${publishedTime}`,
+      ],
+    },
+  };
+}
+
+export default function Post({ params }: { params: { slug: string } }) {
+  const post = getBlogPostBySlug(params.slug);
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <body className="mx-4 mb-40 mt-8 flex max-w-2xl flex-col antialiased md:flex-row lg:mx-auto">
+      <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
+        <section>
+          <script
+            type="application/ld+json"
+            suppressHydrationWarning
+            dangerouslySetInnerHTML={{
+              __html: JSON.stringify({
+                "@context": "https://schema.org",
+                "@type": "BlogPosting",
+                headline: post.metadata.title,
+                datePublished: post.metadata.date,
+                dateModified: post.metadata.date,
+                description: post.metadata.description,
+                image: `https://da.niel.lol/og/blog?title=${
+                  post.metadata.title
+                }&top=${formatDate(post.metadata.date)}`,
+                url: `https://da.niel.lol/blog/${post.slug}`,
+                author: {
+                  "@type": "Person",
+                  name: "Dan",
+                },
+              }),
+            }}
+          />
+
+          <Link
+            href="/blog"
+            className="mb-4 p-2 bg-neutral-900 text-white rounded inline-block"
+          >
+            <Undo2 className="size-5" />
+          </Link>
+
+          <h1 className="title mb-2 max-w-[650px] text-3xl font-medium tracking-tighter">
+            {post.metadata.title}
+          </h1>
+          <div className="mb-8 flex max-w-[650px] items-center justify-between text-sm">
+            <p className="text-sm text-neutral-600 dark:text-neutral-400">
+              {formatDate(post.metadata.date)}
+            </p>
+          </div>
+
+          <article className="prose prose-neutral dark:prose-invert">
+            <MDX source={post.content} />
+          </article>
+        </section>
+      </main>
+    </body>
+  );
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,67 @@
+import type { Metadata } from "next";
+import { Link } from "next-view-transitions";
+import { getBlogPosts } from "../../utils/blog";
+import { Undo2 } from "lucide-react";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description: "Talking about nerdy stuff that interests me.",
+  openGraph: {
+    images: [
+      {
+        url: "https://da.niel.lol/og/home?title=dan's+blog",
+      },
+    ],
+  },
+};
+
+export default function BlogPage() {
+  const posts = getBlogPosts().sort(
+    (a, b) =>
+      new Date(b.metadata.date).getTime() - new Date(a.metadata.date).getTime()
+  );
+
+  return (
+    <body className="mx-4 mb-40 mt-8 flex max-w-2xl flex-col antialiased md:flex-row lg:mx-auto">
+      <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
+        <section>
+          <Link
+            href="/"
+            className="mb-4 p-2 bg-neutral-900 text-white rounded inline-block"
+          >
+            <Undo2 className="size-5" />
+          </Link>
+          <h1 className="text-2xl font-medium tracking-tighter">blog</h1>
+
+          <div className="flex flex-col gap-8">
+            {posts.map((post) => (
+              <Link
+                key={post.slug}
+                href={`/blog/${post.slug}`}
+                className="group"
+              >
+                <div className="flex w-full flex-col gap-y-1">
+                  <p className="text-lg font-medium group-hover:underline group-hover:decoration-neutral-400 group-hover:underline-offset-4 group-hover:dark:decoration-neutral-600">
+                    {post.metadata.title.toLowerCase()}
+                  </p>
+                  <p className="prose prose-neutral dark:prose-invert">
+                    {post.metadata.description.toLowerCase()}
+                  </p>
+                  <p className="text-sm text-neutral-600 dark:text-neutral-400">
+                    {new Date(post.metadata.date)
+                      .toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                        year: "numeric",
+                      })
+                      .toLowerCase()}
+                  </p>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </section>
+      </main>
+    </body>
+  );
+}
