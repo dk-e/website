c66c128ffdcb1e379ba645d606a277003937669c
dan <d@zurly.me>
2024-07-09 18:17:44 +0100

    new layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,8 +5,33 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dan's Website",
-  description: "my personal site",
+  metadataBase: new URL("https://da.niel.lol"),
+  title: {
+    default: "Dan",
+    template: "%s | Dan",
+  },
+  description: "Developer, cardist and maker of things.",
+  openGraph: {
+    title: "Dan",
+    description: "Developer, cardist and maker of things.",
+    url: "https://da.niel.lol",
+    siteName: "Dan",
+    locale: "en_US",
+    type: "website",
+    images: ["https://da.niel.lol/og/home"],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    "max-video-preview": -1,
+    "max-image-preview": "large",
+    "max-snippet": -1,
+  },
+  twitter: {
+    title: "Dan",
+    card: "summary_large_image",
+    creator: "@nexxeln",
+  },
 };
 
 export default function RootLayout({
