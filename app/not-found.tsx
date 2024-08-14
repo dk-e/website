5ea92e929f747b6fa9f3bf4e24ec6a57f2f2230c
dan <d@zurly.me>
2024-08-14 16:49:53 +0100

    fix hydration error

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -16,29 +16,27 @@ export const metadata: Metadata = {
 
 export default function NotFoundPage() {
   return (
-    <body className="mx-4 mb-40 mt-8 flex max-w-2xl flex-col antialiased md:flex-row lg:mx-auto">
-      <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
-        <section>
-          <Link
-            href="/"
-            className="mb-4 p-2 bg-neutral-900 text-white rounded inline-block"
-          >
-            <Undo2 className="size-5" />
-          </Link>
-          <h1 className="text-2xl font-medium tracking-tighter">
-            404 - Page Not Found
-          </h1>
-          <p className="mt-2 text-lg text-neutral-600 dark:text-neutral-400">
-            Sorry, the page you&apos;re looking for doesn&apos;t exist.
-          </p>
-          <Link
-            href="/"
-            className="mt-4 inline-block p-2 bg-neutral-900 text-white rounded"
-          >
-            Go back home
-          </Link>
-        </section>
-      </main>
-    </body>
+    <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
+      <section>
+        <Link
+          href="/"
+          className="mb-4 p-2 bg-neutral-900 text-white rounded inline-block"
+        >
+          <Undo2 className="size-5" />
+        </Link>
+        <h1 className="text-2xl font-medium tracking-tighter">
+          404 - Page Not Found
+        </h1>
+        <p className="mt-2 text-lg text-neutral-600 dark:text-neutral-400">
+          Sorry, the page you&apos;re looking for doesn&apos;t exist.
+        </p>
+        <Link
+          href="/"
+          className="mt-4 inline-block p-2 bg-neutral-900 text-white rounded"
+        >
+          Go back home
+        </Link>
+      </section>
+    </main>
   );
 }
