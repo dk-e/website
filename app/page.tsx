5da63f5d66531c04f80e1327acab9496cb7d40d4
dan <d@zurly.me>
2024-11-06 21:59:33 +0000

    format

--- app/page.tsx
+++ app/page.tsx
@@ -26,7 +26,7 @@ function LocationIcon() {
       strokeWidth="2"
       strokeLinecap="round"
       strokeLinejoin="round"
-      className="h-5 w-5"
+        className="h-5 w-5"
     >
       <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
       <circle cx="12" cy="10" r="3" />
