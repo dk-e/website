f8501e4d742e4196432a2d690612bfeef30f147d
dan <d@zurly.me>
2024-11-08 22:53:45 +0000

    comment out flag

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { Link } from "next-view-transitions";
 import { getBlogPosts } from "../lib/blog";
-import Flag from "react-flagkit";
+// import Flag from "react-flagkit";
 import {
   age,
   daysSinceStartedCoding,
@@ -14,25 +14,25 @@ import { FaInstagram, FaGithub } from "react-icons/fa";
 import { BsTwitterX } from "react-icons/bs";
 import Discord from "../components/discord";
 
-function LocationIcon() {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      className="h-5 w-5"
-    >
-      <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
-      <circle cx="12" cy="10" r="3" />
-    </svg>
-  );
-}
+// function LocationIcon() {
+//   return (
+//     <svg
+//       xmlns="http://www.w3.org/2000/svg"
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       stroke="currentColor"
+//       strokeWidth="2"
+//       strokeLinecap="round"
+//       strokeLinejoin="round"
+//       className="h-5 w-5"
+//     >
+//       <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
+//       <circle cx="12" cy="10" r="3" />
+//     </svg>
+//   );
+// }
 
 const Tooltip = ({ text, children }: any) => (
   <span className="relative group">
