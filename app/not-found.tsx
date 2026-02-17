eaf9e870976e6e14563593ac57f61aa939ec323c
dan <d@niel.lol>
2026-02-17 20:54:40 +0000

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -16,26 +16,24 @@ export const metadata: Metadata = {
 
 export default function NotFoundPage() {
   return (
-    <main className="mt-6 flex min-w-0 flex-auto flex-col px-2 md:px-0">
-      <section>
+    <main className="space-y-8 text-left">
+      <section className="space-y-3">
         <Link
           href="/"
-          className="mb-4 p-2 bg-neutral-900 text-white rounded inline-block"
+          className="group inline-flex items-center gap-1 text-sm text-purple-500 transition-colors hover:text-purple-400 dark:text-purple-400 dark:hover:text-purple-200"
         >
-          <Undo2 className="size-5" />
+          <Undo2 className="h-4 w-4 transition-transform duration-200 group-hover:-translate-x-1" />
+          <span>back</span>
         </Link>
-        <h1 className="text-2xl font-medium tracking-tighter">
-          404 - Page Not Found
+
+        <h1 className="flex items-center gap-1 font-medium uppercase tracking-[0.2em] text-neutral-600 dark:text-neutral-400">
+          <span className="text-purple-500 dark:text-purple-400">*</span>
+          <span>404 - page not found</span>
         </h1>
-        <p className="mt-2 text-lg text-neutral-600 dark:text-neutral-400">
-          Sorry, the page you&apos;re looking for doesn&apos;t exist.
+
+        <p className="max-w-prose text-base text-neutral-700 dark:text-neutral-300">
+          this page does not exist.
         </p>
-        <Link
-          href="/"
-          className="mt-4 inline-block p-2 bg-neutral-900 text-white rounded"
-        >
-          Go back home
-        </Link>
       </section>
     </main>
   );
