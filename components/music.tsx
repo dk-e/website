216a8aebdc487c3efda9a797c81677a960c936d1
dan <d@niel.lol>
2026-09-05 23:51:31 +0100

    remove unused image

--- components/music.tsx
+++ components/music.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import useSWR from "swr";
-import Image from "next/image";
 import { Link } from "next-view-transitions";
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
