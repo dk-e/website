3e92ce77a6576620c2d1c859f0c0e3371fbb9fd9
dan <d@niel.lol>
2026-09-05 21:42:31 +0100

    domain update

--- lib/constants.ts
+++ lib/constants.ts
@@ -12,7 +12,7 @@ export const RelativeTimeFormatter = new Intl.RelativeTimeFormat("en", {
 export const discordId = "745631824163766412";
 export const discordUser = "sexuals";
 
-export const domain = "dann.lol";
+export const domain = "dann.my";
 
 export const dob = new Date("2007-02-23");
 export const age = new Date(Date.now() - dob.getTime()).getUTCFullYear() - 1970;
